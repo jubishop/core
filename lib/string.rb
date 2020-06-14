05faccfcdc8feb48471823b9dbbb6cc93c8654dc
Justin Bishop <jubishop@gmail.com>
2020-06-14 11:55:58 -0700

    rubocop and smarts

--- lib/string.rb
+++ lib/string.rb
@@ -1,7 +1,24 @@
 class String
-  def pluralize(amount = 2)
-    return self if amount == 1
+  include Enumerable
 
-    return self[-1] == 'y' ? "#{self[...-1]}ies" : "#{self}s"
+  VOWELS = %w[a e i o u y].freeze
+  private_constant :VOWELS
+
+  def each
+    return to_enum(:each) unless block_given?
+
+    each_char { |char| yield char }
+  end
+
+  def vowel?
+    all? { |char| VOWELS.include?(char) }
+  end
+
+  def pluralize(amount = 2)
+    return self if amount == 1 || empty?
+
+    return "#{self}s" if length == 1 || self[-1] != 'y' || self[-2].vowel?
+
+    return "#{self[...-1]}ies"
   end
 end
