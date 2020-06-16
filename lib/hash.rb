4ac0434a0099197d4a4f9955346b4f6372d78d23
Justin Bishop <jubishop@gmail.com>
2020-06-15 21:09:03 -0700

    add symbolize_keys

--- lib/hash.rb
+++ lib/hash.rb
@@ -1,4 +1,12 @@
 class Hash
+  def symbolize_keys
+    transform_keys(&:to_sym)
+  end
+
+  def symbolize_keys!
+    transform_keys!(&:to_sym)
+  end
+
   def deep_transform_keys!(&block)
     transform_keys!(&block)
     values.each { |value|
