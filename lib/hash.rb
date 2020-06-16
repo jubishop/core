3ad15aeee4797bd8f95d08b092e2971d24fd30c7
Justin Bishop <jubishop@gmail.com>
2020-06-16 14:47:50 -0700

    remove duration, and update deep_transform_keys! to be better

--- lib/hash.rb
+++ lib/hash.rb
@@ -1,26 +1,15 @@
 class Hash
+  def deep_transform_keys!(&block)
+    transform_keys!(&block)
+    values.deep_transform_keys!(&block)
+    self
+  end
+
   def symbolize_keys
     transform_keys(&:to_sym)
   end
 
   def symbolize_keys!
     transform_keys!(&:to_sym)
-  end
-
-  def deep_transform_keys!(&block)
-    transform_keys!(&block)
-    values.each { |value|
-      if value.is_a?(Hash)
-        value.deep_transform_keys!(&block)
-      elsif value.is_a?(Enumerable)
-        value.each { |element|
-          element.deep_transform_keys!(&block) if element.is_a?(Hash)
-        }
-      end
-    }
-  end
-
-  def deep_symbolize_keys!
-    deep_transform_keys!(&:to_sym)
   end
 end
