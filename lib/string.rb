77f8f8d51a784c5438d46ff7cd73a22330629c3d
Justin Bishop <jubishop@gmail.com>
2021-04-29 15:21:29 -0700

    make rubocop happy

--- lib/string.rb
+++ lib/string.rb
@@ -2,9 +2,7 @@ class String
   VOWELS = %w[a e i o u y].freeze
   private_constant :VOWELS
 
-  PLURAL_MAP = {
-    person: 'people'
-  }.freeze
+  PLURAL_MAP = { person: 'people' }.freeze
   private_constant :PLURAL_MAP
 
   def vowel?
