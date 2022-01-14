6bbd8a9b3653b2759d49271f0e5123366cc76876
Justin Bishop <jubishop@gmail.com>
2022-01-13 16:39:19 -0800

    just make rubocop happy

--- lib/proc.rb
+++ lib/proc.rb
@@ -1,7 +1,7 @@
 class Proc
   def run(*params)
     return call(*params)
-  rescue LocalJumpError => e
-    return e.exit_value
+  rescue LocalJumpError => error
+    return error.exit_value
   end
 end
